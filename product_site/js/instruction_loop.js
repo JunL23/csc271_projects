777d62c3186ee7f5b9039ab16cae03c4ebe4028d
Junrong Liu <junliuwesterly@gmail.com>
2024-11-14 18:14:56 -0500

    Added event listener to the download page

--- product_site/js/instruction_loop.js
+++ product_site/js/instruction_loop.js
@@ -5,10 +5,6 @@ var linux = ['Download Linux installer on the website.', 'Follow the installer p
 var android = ['Download Android installer on the website.', 'Follow the installer prompt.', 'You can now use the app on phone!'];
 var ios = ['Download IOS installer on the website.', 'Follow the installer prompt.', 'You can now use the app on phone!'];
 
-
-// Get the message element that displays message 
-var new_message = document.querySelector('#download-tutorial');
-
 var installer = document.querySelectorAll('.boxes');
 var windows_installer = installer[0];
 var mac_installer = installer[1];
@@ -16,84 +12,49 @@ var linux_installer = installer[2];
 var android_installer = installer[3];
 var ios_installer = installer[4];
 
-var instruction = document.querySelectorAll('.text');
-var list = document.querySelector('#list');
+// Function to customize the install instruction on different operating system
+function install(system_name, instruction_list){
+    // Get the message element that displays message 
+    var new_message = document.querySelector('#download-tutorial');
 
-// If winodws download is pressed
-windows_installer.addEventListener('click', () => {
+    // Get the list step element and the instruction element
+    var list = document.querySelector('#list');
+    var instruction = document.querySelectorAll('.text');
+
     // Change message and its visibility
-    new_message.textContent = "Download instruction for windows";
+    new_message.textContent = "Download instruction for " + system_name + ".";
     list.style.display = 'block';
 
     // Use a while loop to loop through the instruction and add it to the list
     // I use while loop because this operation is simple and while loop offers a simple ways of writing the loop
     var i = 0;
-    while(i < windows.length){
-        instruction[i].textContent = windows[i];
+    while(i < instruction_list.length){
+        instruction[i].textContent = instruction_list[i];
         i ++;
     }
+}
+
+// If winodws download is pressed
+windows_installer.addEventListener('click', () => {
+    install("Windows", windows);
 })
 
 // If MacOS download is pressed
 mac_installer.addEventListener('click', () => {
-    
-    // Change message and its visibility
-    new_message.textContent = "Download instruction for MacOS";
-    list.style.display = 'block';
-
-    // Use a while loop to loop through the instruction and add it to the list
-    // I use while loop because this operation is simple and while loop offers a simple ways of writing the loop
-    var i = 0;
-    while(i < mac.length){
-        instruction[i].textContent = mac[i];
-        i ++;
-    }
+    install("MacOS", mac);
 })
 
 // If Linux download is pressed
 linux_installer.addEventListener('click', () => {
-
-    // Change message and its visibility
-    new_message.textContent = "Download instruction for Linux";
-    list.style.display = 'block';
-
-    // Use a while loop to loop through the instruction and add it to the list
-    // I use while loop because this operation is simple and while loop offers a simple ways of writing the loop
-    var i = 0;
-    while(i < linux.length){
-        instruction[i].textContent = linux[i];
-        i ++;
-    }
+    install("Linux", linux);
 })
 
 // If android download is pressed
 android_installer.addEventListener('click', () => {
-
-    // Change message and its visibility
-    new_message.textContent = "Download instruction for Android";
-    list.style.display = 'block';
-
-    // Use a while loop to loop through the instruction and add it to the list
-    // I use while loop because this operation is simple and while loop offers a simple ways of writing the loop
-    var i = 0;
-    while(i < android.length){
-        instruction[i].textContent = android[i];
-        i ++;
-    }
+    install("Android", android);
 })
 
 // If IOS download is pressed
 ios_installer.addEventListener('click', () => {
-
-    // Change message and its visibility
-    new_message.textContent = "Download instruction for IOS";
-    list.style.display = 'block';
-
-    // Use a while loop to loop through the instruction and add it to the list
-    // I use while loop because this operation is simple and while loop offers a simple ways of writing the loop
-    var i = 0;
-    while(i < ios.length){
-        instruction[i].textContent = ios[i];
-        i ++;
-    }
+    install("IOS", ios);
 })
